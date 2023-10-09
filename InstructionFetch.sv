973b0de388b72c3ee1c89cb60068167564cd2c0a
Anirudh N S <pes2202101128@pesu.pes.edu>
2023-10-09 19:55:59 +0530

    Add files via upload

---
+++ InstructionFetch.sv
@@ -0,0 +1,24 @@
+module InstructionFetch(
+    input bit clk,            
+    output bit [31:0] instruction 
+);
+
+    bit [31:0] pc_next;  
+
+    ProgramCounter PC (
+        .clk(clk),
+        .pc_sel(2'b00),      
+        .imm(32'h0),         
+        .branch_taken(1'b0), 
+        .jump(1'b0),         
+        .pc_out(pc_next)     
+    );
+
+
+    MyMemory memory (
+       .clk(clk),
+       .address(pc_next),
+       .data(instruction)
+    );
+
+endmodule
