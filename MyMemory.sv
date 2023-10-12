316aacdfd2995255346bc2d74c425b88039a9334
Anirudh N S <pes2202101128@pesu.pes.edu>
2023-10-12 19:52:51 +0530

    Update MyMemory.sv

--- MyMemory.sv
+++ MyMemory.sv
@@ -1,13 +1,24 @@
 module MyMemory(
-    input bit clk,        
-    input bit [31:0] address, 
-    output bit [31:0] data 
+    input bit [31:0] pc,                 
+    output bit [31:0] instr,
+	 input bit clk
 );
-	 // Considering a 16 bit range
-    reg [31:0] memory_array [-32768:32767];
+
+    reg [31:0] mem [0:32768]; 
+
+   
+    initial begin
+
+        mem[0] = 32'b00000110000101000000110010010011;
+        mem[4] = 32'b00000001000101000100001100010011;
+	mem[8] = 32'b01000000000000000110000110010011;
+	mem[12] = 32'b00000110010010000000000010000011;
+	mem [16] = 32'b00000010000110000110000110010011;
+        
+    end
 
     always @(posedge clk) begin
-        data <= memory_array[address];
+        instr = mem[pc >> 2]; 
     end
 
 endmodule
