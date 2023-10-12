6b98444a1a60dd410aa11e837ea98aa4cc5afd6e
Anirudh N S <pes2202101128@pesu.pes.edu>
2023-10-12 19:15:29 +0530

    Update ProgramCounter.sv

--- ProgramCounter.sv
+++ ProgramCounter.sv
@@ -1,23 +1,27 @@
-module ProgramCounter(
-    input bit clk,          
-    input bit [1:0] pc_sel,  
-    input bit [31:0] imm,           
-    output bit [31:0] pc_out  
+module Program_Counter(
+    input bit clk,                 
+    input bit signed [31:0] imm,          
+    input bit branch,              
+    input bit zero_flag,          
+    output bit [31:0] pc_out       
 );
 
+
+    bit [31:0] pc_reg;              
+    bit [31:0] pc_next;            
+    bit branch_taken;              
+
+    assign branch_taken = branch && zero_flag;
+
     always @(posedge clk) begin
-        if (pc_sel == 2'b11) begin
-            pc_out <= 32'b0; 
-        end 
-		  else if (pc_sel == 2'b00) begin 
-            pc_out <= pc_out + 4; 
-        end 
-		  else if (pc_sel == 2'b01) begin 
-            pc_out <= pc_out + imm; 
-        end 
-		  else if (pc_sel == 2'b10) begin 
-            pc_out <= imm; 
+        if (branch_taken) begin
+            pc_reg <= pc_reg + imm;  
+        end else begin
+            pc_reg <= pc_reg + 4;    
         end
     end
-	 
+
+    assign pc_next = branch_taken ? pc_reg + imm : pc_reg + 4;
+    assign pc_out = pc_next;
+
 endmodule
